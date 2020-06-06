a90360fee1bc1cae767a29727a411cef311fd499
unknown <18234096788@sina.cn>
2020-06-06 23:46:29 +0800

    2020-6-6-2

---
+++ build/precache-manifest.5a60eddac69cd4eec8c5eeb5a9a25c3e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e437b540c1c23eae1e4771b3f29db7f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c96ebfa00d5a87c8d669",
+    "url": "/static/css/main.2c2921a0.chunk.css"
+  },
+  {
+    "revision": "d430acc6dd15e993e3fb",
+    "url": "/static/js/2.8dc1f9c5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8dc1f9c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96ebfa00d5a87c8d669",
+    "url": "/static/js/main.82273f67.chunk.js"
+  },
+  {
+    "revision": "2d72dc5fa4c6ddf01895",
+    "url": "/static/js/runtime-main.2936372e.js"
+  },
+  {
+    "revision": "79388eabc780eed4a341bc9e55f01e48",
+    "url": "/static/media/Abdul.79388eab.jpg"
+  },
+  {
+    "revision": "eb6e6c51b5c3129b32131aaedfeeb302",
+    "url": "/static/media/Catherine.eb6e6c51.jpg"
+  },
+  {
+    "revision": "9697ea3120937c5b9cd320f85c353aaf",
+    "url": "/static/media/Dusan.9697ea31.jpg"
+  },
+  {
+    "revision": "0b2ead88525c409410584154c381a8d5",
+    "url": "/static/media/Lee.0b2ead88.jpg"
+  },
+  {
+    "revision": "e5e3c86c58a9afe9370465e355367090",
+    "url": "/static/media/english-dad.e5e3c86c.png"
+  },
+  {
+    "revision": "450eb816651c48dfe9bcd7adc58c1d79",
+    "url": "/static/media/learnEnglish.450eb816.png"
+  },
+  {
+    "revision": "75abbcc4a29e064dd159e7817d5e32a0",
+    "url": "/static/media/logo.75abbcc4.jpg"
+  },
+  {
+    "revision": "295aae71bd67794ce6daf83c94aafb19",
+    "url": "/static/media/map.295aae71.png"
+  },
+  {
+    "revision": "3263e14b4771afa34d439e9839f9dc29",
+    "url": "/static/media/teacher.3263e14b.png"
+  },
+  {
+    "revision": "14f4b9590519b70e8e14848a59fe5695",
+    "url": "/static/media/wy1.14f4b959.png"
+  },
+  {
+    "revision": "c2dea61a1e7a4d16518ff01c774a2778",
+    "url": "/static/media/wy2.c2dea61a.png"
+  },
+  {
+    "revision": "5b4e95e5f0cbd5b1a211ea3d7a586a71",
+    "url": "/static/media/wy3.5b4e95e5.png"
+  },
+  {
+    "revision": "d58874337ce26e61f4cbfbd7d1d5d32a",
+    "url": "/static/media/wy4.d5887433.png"
+  }
+]);
